3d9013e739f4cacd0d4024eaa08e3f688a70f57e
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2016-05-21 13:51:17 -0300

    Change to readFileSync

--- routes/schema.js
+++ routes/schema.js
@@ -12,6 +12,7 @@ router.get('/schema', function (req, res, next) {
       res.send({type: 'connect', message: 'error', error: err});
       process.exit(1);
     }
+    console.log('Connected to postgres! Running schemas...');
     processSQLFile(path.resolve(__dirname, 'session.sql'), client, res);
   });
 });
@@ -19,16 +20,18 @@ router.get('/schema', function (req, res, next) {
 function processSQLFile (fileName, client, res) {
   console.log('fileName', fileName);
   // Extract SQL queries from files. Assumes no ';' in the fileNames
-  var queries = fs.readFile(fileName).toString()
+  var queries = fs.readFileSync(fileName).toString()
     .replace(/(\r\n|\n|\r)/gm," ") // remove newlines
     .replace(/\s+/g, ' ') // excess white space
     .split(";") // split into all statements
     .map(Function.prototype.call, String.prototype.trim)
     .filter(function (el) { return el.length != 0 }); // remove any empty ones
 
+  console.log('Starting executing querys');
   // Execute each SQL query sequentially
   queries.forEach(function (query) {
     batch.push(function (done) {
+      console.log('Executing query = ', query);
       client.query(query, function (err, result) {
         done();
         if (err) {
